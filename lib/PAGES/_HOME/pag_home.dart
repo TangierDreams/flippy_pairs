e0e658e1b2a46b7c470e5f5805adbaa331e47188
Cocinero Ryback <luistanger@gmail.com>
2025-09-23 00:09:35 +0200

    23/09/2025

--- lib/PAGES/_HOME/pag_home.dart
+++ lib/PAGES/_HOME/pag_home.dart
@@ -1,12 +1,17 @@
 import 'package:flippy_pairs/SHARED/UTILS/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flippy_pairs/SHARED/WIDGETS/wid_toolbar.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+int rowsSelected = 3;
+int colsSelected = 2;
 
 class PagHome extends StatelessWidget {
   const PagHome({super.key});
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       //Toolbar:
       appBar: WidToolbar(
@@ -24,24 +29,87 @@ class PagHome extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            Text(
+              'Difficulty',
+              textAlign: TextAlign.center,
+              style: GoogleFonts.chewy(
+                textStyle: const TextStyle(
+                  fontSize: 30,
+                  height: 0.7,
+                  color: Colors.orange,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 4,
+                      color: Colors.black54,
+                      offset: Offset(2, 2),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 15),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 GridButton(pTitle: "3x2", pRows: 3, pCols: 2),
                 GridButton(pTitle: "4x3", pRows: 4, pCols: 3),
                 GridButton(pTitle: "5x4", pRows: 5, pCols: 4),
-              ]
+              ],
             ),
+
             const SizedBox(height: 20),
+
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 GridButton(pTitle: "6x5", pRows: 6, pCols: 5),
                 GridButton(pTitle: "8x7", pRows: 8, pCols: 7),
                 GridButton(pTitle: "9x8", pRows: 9, pCols: 8),
-              ]
-            )
+              ],
+            ),
 
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).pushNamed('/game', arguments: {'pRows': rowsSelected, 'pCols': colsSelected});
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 15,
+                ),
+                backgroundColor: AppColors.primary,
+                foregroundColor: AppColors.contrast,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                elevation: 10,
+              ),
+
+              child: Text(
+                'Start Playing!',
+                style: GoogleFonts.luckiestGuy(
+                  textStyle: const TextStyle(
+                    fontSize: 28,
+                    //height: 0.9,
+                    color: Colors.orange,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 8,
+                        color: Colors.black87,
+                        offset: Offset(3, 3),
+                      ),
+                    ],
+                  ),
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
           ],
         ),
       ),
@@ -54,29 +122,42 @@ class GridButton extends StatelessWidget {
   final int pRows;
   final int pCols;
 
-  const GridButton({super.key, required this.pTitle, required this.pRows, required this.pCols});
+  const GridButton({
+    super.key,
+    required this.pTitle,
+    required this.pRows,
+    required this.pCols,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        Navigator.of(
-          context,
-        ).pushNamed('/game', arguments: {'pRows': pRows, 'pCols': pCols});
-      },
-      style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 15),
+        onPressed: () {
+            rowsSelected = pRows;
+            colsSelected = pCols;
+        },      
+        style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 20),
         backgroundColor: AppColors.primary,
         foregroundColor: AppColors.contrast,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-        elevation: 8,
+        elevation: 10,
       ),
       child: Text(
         pTitle,
-        style: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w700,
-          letterSpacing: 0.5,
+        style: GoogleFonts.chewy(
+          textStyle: const TextStyle(
+            fontSize: 30,
+            height: 0.7,
+            color: Colors.yellow,
+            shadows: [
+              Shadow(
+                blurRadius: 4,
+                color: Colors.black54,
+                offset: Offset(2, 2),
+              ),
+            ],
+          ),
         ),
         textAlign: TextAlign.center,
       ),
